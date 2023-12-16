520e9415d8101840e61e436299a1a536c600bc0f
ae-GYEONGWON <tjsxor50@gmail.com>
2023-12-16 16:13:46 +0900

    feat: JWT를 위한 환경 변수 각 함수에 추가

--- app/security/jwt.py
+++ app/security/jwt.py
@@ -18,16 +18,18 @@ oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
 
 # JWT 토큰 생성 함수
 def create_access_token(data: dict, expires_delta: timedelta = None):
+    global SECRET_KEY, ALGORITHM
     to_encode = data.copy()
     if expires_delta:
         expire = datetime.utcnow() + expires_delta
     else:
-        expire = datetime.utcnow() + timedelta(minutes=15)
+        expire = datetime.utcnow() + timedelta(minutes=30)
     to_encode.update({"exp": expire})
     encoded_jwt = jwt.encode(to_encode, SECRET_KEY, algorithm=ALGORITHM)
     return encoded_jwt
 
 def get_jwt(user_id):
+    global ACCESS_TOKEN_EXPIRE_MINUTES
     # JWT 토큰 생성
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     token_data = {"sub": user_id}
@@ -36,11 +38,15 @@ def get_jwt(user_id):
 
 # JWT 토큰 검증 함수
 def verify_token(token: str):
+    global SECRET_KEY, ALGORITHM
     try:
+        print("#######")
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
-    
+        print("$$$$$$$$$$$$$$$$$")
         user_id: str = payload.get("sub")
+        print("*******************")
         exp_time = payload.get("exp")
+        print("!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
 
         if user_id is None:
             return None, None
@@ -51,9 +57,11 @@ def verify_token(token: str):
     return token_data, exp_time
 
 # JWT 토큰을 사용하여 사용자 확인 함수
-async def get_current_user(token: str = Depends(oauth2_scheme)):
+async def get_current_user(token: str):
+    global ACCESS_TOKEN_EXPIRE_MINUTES
     # 토큰 디코딩 및 사용자 확인 로직 작성
     token_data, exp_time = verify_token(token)
+    print(token_data)
     if token_data is None:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,

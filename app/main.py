63eb67f9fdbe2e13cb00b4cec39fd13dba47ed4b
ae-GYEONGWON <tjsxor50@gmail.com>
2023-12-19 23:06:30 +0900

    modify: cors origin 수정

--- app/main.py
+++ app/main.py
@@ -42,6 +42,7 @@ origins = [
     "http://localhost:3000",
     "http://be-be-c957f-21216619-aeb7ba37580c.kr.lb.naverncp.com",
     "http://ing-fe-redirectingress-b09e0-21395882-5398597ca2af.kr.lb.naverncp.com"
+    "http://www.foodteacher.xyz/"
 ]
 app.add_middleware(
     CORSMiddleware,

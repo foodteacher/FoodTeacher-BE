e279adc9ea37d0717687f0246b34da3592527b65
ae-GYEONGWON <tjsxor50@gmail.com>
2024-01-04 19:51:47 +0900

    modify: user regist 로직 수정

--- app/crud/user.py
+++ app/crud/user.py
@@ -4,7 +4,7 @@ from sqlalchemy.orm import Session
 
 from app.crud.base import CRUDBase
 from app.models.user import User
-from app.schemas.user import UserCreate, UserUpdate
+from app.schemas.user import UserCreate, UserUpdate, UserRegist
 
 
 class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
@@ -19,6 +19,13 @@ class CRUDUser(CRUDBase[User, UserCreate, UserUpdate]):
         db.commit()
         db.refresh(db_obj)
         return db_obj
+    
+    def regist(self, db: Session, *, db_obj: User, obj_in: Union[UserRegist, Dict[str, Any]]) -> User:
+        if isinstance(obj_in, dict):
+            update_data = obj_in
+        else:
+            update_data = obj_in.dict(exclude_unset=True)
+        return super().update(db, db_obj=db_obj, obj_in=update_data)
 
     # def update(self, db: Session, *, db_obj: User, obj_in: Union[UserUpdate, Dict[str, Any]]) -> User:
     #     if isinstance(obj_in, dict):

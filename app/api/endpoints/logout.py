e6d622e1c0386f0bf689b2bc282ed1ae5cecdcae
ae-GYEONGWON <tjsxor50@gmail.com>
2024-01-07 15:33:51 +0900

    feat: 로그아웃시 카카오 계정 로그아웃 기능 추가

--- app/api/endpoints/logout.py
+++ app/api/endpoints/logout.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.orm import Session
 from app.db.session import get_db
 
@@ -7,11 +7,29 @@ from app.api.depends import get_current_user
 from app.crud.user import crud_user
 from app.models.user import User
 
+import requests
+
 
 router = APIRouter()
 settings = get_setting()
 
 @router.post('/logout')
 def logout(db: Session = Depends(get_db), current_user: User = Depends(get_current_user)):
+    kakao_logout(current_user=current_user)
     res = crud_user.remove_field(db=db, db_obj=current_user, field="refresh_token")
     return res
+
+def kakao_logout(current_user: User):
+    access_token = current_user.kakao_access_token
+    _url = f"https://kapi.kakao.com/v1/user/logout"
+    headers = {
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Authorization": f"Bearer {access_token}"
+    }
+    _res = requests.post(_url, headers=headers)
+    
+    if _res.status_code == 200:
+        _result = _res.json()
+        return _result
+    else:
+        raise HTTPException(status_code=401, detail="Kakao logout failed")
